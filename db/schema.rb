421e9232b2d2a48cda7d3be1216ec38e3edd52c6
Miguel <mige.d@outlook.com>
2022-01-04 16:36:22 -0500

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625062916) do
+ActiveRecord::Schema.define(version: 20220104205647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,16 +54,16 @@ ActiveRecord::Schema.define(version: 20160625062916) do
     t.integer  "category_id"
   end
 
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
-  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
